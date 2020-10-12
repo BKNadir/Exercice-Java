d798a7f20e1e148cc87a561eb81d6a732e33c13d
Nadir Bakkali <nadir.bakkali@outlook.com>
2020-10-12 17:15:22 +0200

    Finished ex17 and started ex18

---
+++ src/iprg/epfc/ex18.java
@@ -0,0 +1,12 @@
+package iprg.epfc;
+
+import java.util.Scanner;
+
+public class ex18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduisez deux nombres");
+        double a = sc.nextDouble(), b = sc.nextDouble();
+        if ((a > b  && a > -b) || ( b > a && b > -a))
+    }
+}
